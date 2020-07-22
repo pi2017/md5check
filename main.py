63db829b03fc15dafc7ed5bc4cbe8141ecbaf394
dev_21 <o.savchenko@ukrspecsystems.com>
2020-07-22 17:04:35 +0300

    MD5 checksum

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+MD5 checsum checker for large file
+22.07.2020
+Author: Savchenko O.V.
+Company Makerobot
+'''
+import hashlib
+import pathlib
+
+filename = input("Enter the file name: ")
+file = pathlib.Path(filename)
+if file.exists():
+    md5_hash = hashlib.md5()
+    with open(file, "rb") as f:
+    # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+            print('MD5 checksum: ', md5_hash.hexdigest())
+else:
+    print("File not exist")
